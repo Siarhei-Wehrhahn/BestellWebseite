be9c05c9638908d92035e403f9a8716077a1c5c0
Sergej <sergei1997.sw@gmail.com>
2024-09-17 20:12:42 +0200

    fehler tritt auf wenn kleiner screen geladen wird ansonsten fertig

--- script.js
+++ script.js
@@ -1,5 +1,117 @@
 function toggleBasket() {
     const basket = document.getElementById('basketId');
-    basket.classList.toggle('d_none');
-    basket.classList.toggle('open');
+    if (window.innerWidth > 620) {
+        basket.classList.toggle('d_none');
+        basket.classList.toggle('open');
+    } else {
+        const basket = document.getElementById('basketId');
+        const cart = document.getElementById('dishCartId');
+        cart.classList.toggle('d_none');
+        basket.classList.toggle('d_none');
+        basket.classList.toggle('open');
+    }
 }
+
+function insertToBasket(dishId) {
+    const selectedDish = restaurant.menu.find(dish => dish.id === dishId);
+    const existingDish = restaurant.cart.items.find(item => item.id === dishId);
+
+    if (existingDish) {
+        existingDish.cartCount++;
+    } else {
+        restaurant.cart.items.push({
+            id: selectedDish.id,
+            name: selectedDish.name,
+            price: selectedDish.price,
+            cartCount: 1
+        });
+    }
+
+    updateTotalPrice();
+    renderBasket();
+}
+
+function removeFromBasket(dishId) {
+    const existingDish = restaurant.cart.items.find(item => item.id === dishId);
+
+    if (existingDish) {
+        existingDish.cartCount--;
+
+        if (existingDish.cartCount == 0) {
+            const index = restaurant.cart.items.indexOf(existingDish);
+            restaurant.cart.items.splice(index, 1);
+        }
+    }
+    updateTotalPrice();
+    renderBasket();
+}
+
+function removeAll(dishId) {
+    const existingDish = restaurant.cart.items.find(item => item.id === dishId);
+    const index = restaurant.cart.items.indexOf(existingDish);
+
+    if (existingDish) {
+        existingDish.cartCount = 0
+    }
+    restaurant.cart.items.splice(index, 1)
+
+    updateTotalPrice();
+    renderBasket();
+}
+
+function updateTotalPrice() {
+    let total = 0;
+
+    for (let i = 0; i < restaurant.cart.items.length; i++) {
+        const element = restaurant.cart.items[i];
+        total += element.price * element.cartCount;
+    }
+
+    restaurant.cart.totalPrice = total.toFixed(2)
+}
+
+function deleteBasket() {
+    restaurant.cart.items = [];
+    restaurant.cart.totalPrice = 0.00;
+}
+
+function order() {
+    for (let index = 0; index < restaurant.cart.items.length; index++) {
+        let cart = restaurant.cart.items[index]
+        let price = restaurant.cart.totalPrice
+
+        let order = {
+            order: cart,
+            price: price
+        }
+        restaurant.orderStatus.orderDetails.unshift(order);
+    }
+
+    restaurant.orderStatus.orderNumber = Date.now().toString();
+    restaurant.orderStatus.deliveryEstimate = "15 min"
+    console.log(restaurant.orderStatus)
+    deleteBasket()
+    updateTotalPrice();
+    renderBasket();
+}
+
+function toggleOverlay() {
+    const overlay = document.getElementById('overlay');
+
+    overlay.classList.toggle('d_flex');
+}
+
+function checkViewportWidth() {
+    const basket = document.getElementById('basketId');
+    if (window.innerWidth < 620 && !basket.classList.contains('d_none')) {
+        basket.classList.add('d_none');
+    } else {
+        return
+    }
+}
+
+// Initial check
+checkViewportWidth();
+
+// Add event listener to handle resizing
+window.addEventListener('resize', checkViewportWidth);
--- script/template.js
+++ script/template.js
@@ -2,67 +2,75 @@ function renderBasket() {
     const basket = document.getElementById('foodListId')
     basket.innerHTML = ""
     for ( let i = 0; i < restaurant.cart.items.length; i++) {
-        const element = restaurant[i];
+        const element = restaurant;
         basket.innerHTML += getBasketList(element, i);
     }
 
     if(restaurant.cart.items.length < 1) {
         basket.innerHTML += getBasketListIfEmpty()
+    } else {
+        basket.innerHTML += getPrice()
     }
+}
+
+function getPrice() {
+    return `
+            <div class="price">
+                <table>
+                    <tr>
+                        <td class="left">Zwischensumme</td>
+                        <td class="right">${restaurant.cart.totalPrice} €</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Lieferkosten</td>
+                        <td class="right">5.00 €</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Gesamt</td>
+                        <td class="right">${(parseFloat(restaurant.cart.totalPrice) + 5.00).toFixed(2)} €</td>
+                    </tr>
+                </table>
+                <button class="orderBtn" onclick="renderMessage()">Bestellen</button>
+            </div>`
 }
 
 function getBasketList(element, i) {
     return /*html*/`
             <div class="food">
-                    <h3>${element.cart.items[i].name}</h3>
-                    <div class="price_count">
-                        <i class="fa-solid fa-minus" style="color: #ff9935;"></i>
-                        <p>${element.cart.items[i].cartCount}x</p>
-                        <i class="fa-solid fa-plus padding_right" style="color: #ff9935;"></i>
-                        <p class="padding_right">${element.cart.items[i].price}</p>
-                        <i class="fa-solid fa-trash-can" style="color: #ff2600;"></i>
-                    </div>
+                <h3>${element.cart.items[i].name}</h3>
+                <div class="price_count">
+                    <i class="fa-solid fa-minus" style="color: #ff9935;" onclick="removeFromBasket(${element.cart.items[i].id})"></i>
+                    <p>${element.cart.items[i].cartCount}x</p>
+                    <i class="fa-solid fa-plus padding_right" style="color: #ff9935;" onclick="insertToBasket(${element.cart.items[i].id})"></i>
+                    <p>${(element.cart.items[i].price * element.cart.items[i].cartCount).toFixed(2)}</p>
+                    <i class="fa-solid fa-trash-can" style="color: #ff9935;" onclick="removeAll(${element.cart.items[i].id})"></i>
                 </div>
-                <div class="price">
-                    <table>
-                        <tr>
-                            <td class="left">Zwischensumme</td>
-                            <td class="right">${element.cart.totalPrice}€</td>
-                        </tr>
-                        <tr>
-                            <td class="left">Lieferkosten</td>
-                            <td class="right">5,00 €</td>
-                        </tr>
-                        <tr>
-                            <td class="left">Gesamt</td>
-                            <td class="right">${element.cart.items[i].cartCount + 5.00}€</td>
-                        </tr>
-                    </table>
-                </div>
+            </div>
+                
     `
 }
 
 function getBasketListIfEmpty() {
     return /*html*/`
             <div class="food">
-                    <h4>Dein Warenkorb ist zurzeit leer!</h4>
-                </div>
-                <div class="price">
-                    <table>
-                        <tr>
-                            <td class="left">Zwischensumme</td>
-                            <td class="right">0.00 €</td>
-                        </tr>
-                        <tr>
-                            <td class="left">Lieferkosten</td>
-                            <td class="right">5,00€</td>
-                        </tr>
-                        <tr>
-                            <td class="left">Gesamt</td>
-                            <td class="right">0.00 €</td>
-                        </tr>
-                    </table>
-                </div>
+                <h4>Dein Warenkorb ist zurzeit leer!</h4>
+            </div>
+            <div class="price">
+                <table>
+                    <tr>
+                        <td class="left">Zwischensumme</td>
+                        <td class="right">0.00 €</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Lieferkosten</td>
+                        <td class="right">5,00 €</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Gesamt</td>
+                        <td class="right">0.00 €</td>
+                    </tr>
+                </table>
+            </div>
     `
 }
 
@@ -79,11 +87,105 @@ function renderKategories() {
 function getCategories(categorie, i) {
     return /*html*/`
     <div>
-        <span id="categoryBtn${i}" class="categoryButton">${categorie}</span>
+        <span id="categoryBtn${i}" class="categoryButton" onclick="scrollToCategory('${categorie}')">${categorie}</span>
     </div>
-    `
+    `;
+}
+
+function scrollToCategory(category) {
+    const categoryElement = document.getElementById(`category-${category}`);
+    if (categoryElement) {
+        categoryElement.scrollIntoView({ behavior: 'smooth' });
+    }
 }
 
 function init() {
-    renderKategories()
+    renderKategories();
+    renderDishCart();
+}
+
+function renderDishCart() {
+    const dishBox = document.getElementById('dishCartFullId');
+    dishBox.innerHTML = "";
+
+    const categoriesRendered = {};  // Ein Objekt, um sicherzustellen, dass jede Kategorie nur einmal gerendert wird
+
+    for (let i = 0; i < restaurant.menu.length; i++) {
+        const menuItem = restaurant.menu[i];
+        const category = menuItem.category;
+
+        if (!categoriesRendered[category]) {
+            categoriesRendered[category] = true;
+            
+            const categoryImage = restaurant.categoryImages[category] || '/images/default.jpg';
+            
+            dishBox.innerHTML += getCategoryHeader(category, categoryImage);
+            
+            for (let j = 0; j < restaurant.menu.length; j++) {
+                const dish = restaurant.menu[j];
+                
+                if (dish.category === category) {
+                    dishBox.innerHTML += getDishCart(dish);
+                }
+            }
+        }
+    }
+}
+
+// Diese Funktion rendert den Header der Kategorie (mit Bild)
+function getCategoryHeader(category, categoryImage) {
+    return /*html*/`
+        <div class="categoryHeader" id="category-${category}">
+            <img class="categoryPhoto" src="${categoryImage}" alt="${category}">
+            <h2>${category}</h2>
+        </div>
+    `;
+}
+
+// Diese Funktion rendert die Gerichte einer Kategorie
+function getDishCart(menuItem) {
+    return /*html*/ `
+        <div class="dishHeaderFull">
+            <div class="dishHeader">
+                <div class="nameAndRating">
+                    <h2>${menuItem.name}</h2>
+                    <div class="ratingWithStar">
+                        <h3>${menuItem.rating}</h3>
+                        <i class="fa-regular fa-star" style="color: rgba(255, 140, 0, 0.8);"></i>
+                    </div>
+                </div>
+                <div class="plus">
+                    <i class="fa-solid fa-plus fa-2xl" style="color: #ff9935;" onclick="insertToBasket(${menuItem.id})"></i>
+                </div>
+            </div>
+            <div class="priceAndDescription">
+                <p class="descriptionDisch">${menuItem.description}</p>
+                <p class="dishPrice">${menuItem.price.toFixed(2)} €</p>
+            </div>
+        </div>
+    `;
+}
+
+function renderOderInfo() {
+    for (let index = 0; index < restaurant.orderStatus.orderDetails.length; index++) {
+        const element = restaurant.orderStatus.orderDetails[index].order;
+        return element
+    }
+}
+
+function renderMessage() {
+    order()
+    const orderStatus = restaurant.orderStatus
+    const basket = document.getElementById('foodListId')
+    basket.innerHTML += /*html*/`
+            <div id="overlay" class="overlay">
+                <div class="overlay-content">
+                    <p id="overlay-message">Testbestellung erfolgreich!</p>
+                    <p>Bestellnummer: ${orderStatus.orderNumber}</p>
+                    <p>Dauer der zubereitung: ${orderStatus.deliveryEstimate}</p>
+                    <button id="closeOverlay" onclick="toggleOverlay()">OK</button>
+                </div>
+            </div>
+    `
+    toggleOverlay()
 }
